dc22d015b3a115ac8d1cf06fdfe07c2aa8e7157d
Daveed21 <dramirez023@student.butte.edu>
2026-09-14 21:07:21 +0000

    Staging brief

---
+++ WeekAssignmentPt1.py
@@ -0,0 +1,4 @@
+#Thinking of doing the blinker of a car through python. Could be easy ish and set a for loop to go for x amount of times printed(Basically showing the flashes)
+
+
+#Dont forget assignemt
