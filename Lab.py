1875d7b2465224d2629425d61150a4368530bdb5
Daveed21 <dramirez023@student.butte.edu>
2026-09-22 15:02:24 -0700

    Clean up Lab.py formatting and remove blank lines
    Removed unnecessary blank lines and cleaned up formatting.

--- Lab.py
+++ Lab.py
@@ -1,22 +1,12 @@
 import random
 
-
 currentDice = random.randint(1,6)
 
 sidesOfADie = [1,2,3,4,5,6]
 
-
-
 print("There are the sides to a die: ", sidesOfADie)
 print("The current die number is: ", currentDice)
-
-
-
-
 
 anotherWayToChooseInAList = random.choice(sidesOfADie)
 
 print("This came from another way to choose within a list: ", anotherWayToChooseInAList)
-
-
-
