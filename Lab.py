74bf519c3f5897e053d449bc7376c238bc138e9f
Daveed21 <dramirez023@student.butte.edu>
2026-09-22 15:02:44 -0700

    Add numpy array and various lists to Lab.py

--- Lab.py
+++ Lab.py
@@ -10,3 +10,20 @@ print("The current die number is: ", currentDice)
 anotherWayToChooseInAList = random.choice(sidesOfADie)
 
 print("This came from another way to choose within a list: ", anotherWayToChooseInAList)
+
+import numpy
+
+happyFace = [1,2,3,4]
+
+smily = numpy.array(happyFace)
+
+# Change
+happyChar = ['1','b','c','d']
+happyChar1 = ["Words", "to", "speak"]
+happyBools = [True, False, True]
+happyBools2 = [[1,2,3], [True, False, True], ["words"]]
+
+print("Hello World")
+print(happyBools2)
+
+print(smily)
