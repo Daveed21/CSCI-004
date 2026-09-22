a451ffffa98051596c468f43ffc8620e36dd33ec
Daveed21 <dramirez023@student.butte.edu>
2026-09-22 02:50:49 +0000

    First edition not finished

---
+++ WeekAssignmentPt2.py
@@ -0,0 +1,37 @@
+# ax^exp + bx + c
+#Starting equation I would like to work on
+
+#Need to see about importing any math libraries in order to incorporate the ^2 better. If not we can do the longer way
+import math
+
+#Create assignments for each part of the equation and get the inputs
+#Also looking at documentation in a quick way to get only an integer input and wondered if I can just put int and the surround the input... Which works nice lol
+print("What is the 'a' assignment")
+aAssignment = int(input('-->'))
+
+print("What is the 'b' assignment")
+bAssignment = int(input('-->'))
+
+print("What is the 'c' assignment")
+cAssignment = int(input('-->'))
+
+print("What is the exponent input")
+expAssignment = int(input('-->'))
+
+print("What is the 'x' input")
+xAssignment = int(input('-->'))
+
+#Now to create our variable in math notation. May be easier to break down each section since it is separated by a '+' symbol
+
+#Part 1
+#mTester = aAssignment ** bAssignment   (Leaving this here for reference that the first one is what gets powered)
+
+
+#Test power multiplier? yeah lets print here
+#Found out by luuck in math.exp that putting two ** symbols gets power.. which makes sense since its times itself neat
+print(mTester)
+#Part 2
+
+
+
+#Part 3
